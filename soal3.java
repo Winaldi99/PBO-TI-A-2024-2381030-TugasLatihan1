9c9435f9e94e133dc8ac020df5920106c62069a0
BATAGOR <2381030@unai.edu>
2024-09-15 20:39:56 +0700

    Commit

---
+++ soal3.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+public class soal3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukan tulisan (camelCase): ");
+        String kata = input.nextLine();
+
+        int jumlahKata = 1;
+
+        for (int i = 0; i < kata.length(); i++) {
+            if (Character.isUpperCase(kata.charAt(i))) {
+                jumlahKata++;
+            }
+        }
+        System.out.println("Jumlah camelCase nya ialah : " + jumlahKata);
+    }
+}
